00aa4d37ec0ca6b64934e66f32d40bf92ec06c45
NishatFariza <nishatfarzia78@gmail.com>
2022-03-19 00:37:51 +0600

    multiple component in different file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <Countries></Countries>
-      
+      <Countries></Countries>
+      <Header></Header>
+
+      {/* <LoadCountries></LoadCountries> */}
     </div>
   );
 }
