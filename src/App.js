62421650629b037694b8e3ac8df7bddf8e75c779
NishatFariza <nishatfarzia78@gmail.com>
2022-03-18 08:06:36 +0600

    setCountries useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+function LoadCountries (){
+   const [countries, setCountries] = useState([])
+  return(
+    <div>
+      <h1>Visiting Every Country Of The World</h1>
+    </div>
+  )
 }
 
 export default App;
