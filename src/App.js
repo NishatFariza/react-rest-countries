6f436f094cefb14dbe4f7d2ced88f249fead66c7
NishatFariza <nishatfarzia78@gmail.com>
2022-03-18 08:37:10 +0600

    display country name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -11,10 +11,27 @@ function App() {
 }
 
 function LoadCountries (){
-   const [countries, setCountries] = useState([])
+   const [countries, setCountries] = useState([]);
+   useEffect(() =>{
+     fetch('https://restcountries.com/v3.1/all')
+     .then(res => res.json())
+     .then(data => setCountries(data))
+
+   }, [])
   return(
     <div>
       <h1>Visiting Every Country Of The World</h1>
+      <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common}></Country>)
+      }
+    </div>
+  )
+}
+function Country (props){
+  return(
+    <div>
+      <h2>Country: {props.name}</h2>
     </div>
   )
 }
